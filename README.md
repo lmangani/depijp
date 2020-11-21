341a6f376b0d059f55bbd56860ad32609a2b6c9b
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2020-11-21 16:44:24 +0100

    alpha flight

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # depijp
 WSS Pipes and Forwarding Echo Chambers
+
+### Install
+```
+npm install -g depijp
+```
+
+### Usage
+##### Using [community relay](https://github.com/meething/ws-multisocket)
+
+##### Using [custom relay](https://github.com/meething/ws-multisocket)
+
+
+#### Chat Example
+Open two (or 100) terminals and run the following:
+```
+depijp {secret_channel_key}
+```
+
+##### Use [Custom WSS Relay](https://github.com/meething/ws-multisocket)
+```
+depijp {secret_channel_key} {wss://your.server:port}
+```
+
+#### Pipe Example
+Open a reading socket, writing to a file:
+```
+depijp {secret_channel_key} > /tmp/mynewfile.txt
+```
+Open a writing socket, reading from a file:
+```
+depijp {secret_channel_key} < /tmp/myoldfile.txt
+```
+
+
+![image](https://user-images.githubusercontent.com/1423657/99881049-acce4000-2c17-11eb-9c5d-c2498f33af86.png)
+
---
+++ depijp.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+var minimist = require('minimist')
+var dedent = require('dedent')
+var websocket = require('websocket-stream')
+var QuickLRU = require('quick-lru');
+const swarm = new QuickLRU({maxSize: 1000});
+
+var utils = require('./utils.js');
+var help = dedent`
+    Usage: echocha <topic> <optional server>
+    Examples:
+      echo hello | echocha test123
+`
+var args = minimist(process.argv.slice(2))
+
+// Join Echo Room
+var uuid = utils.uuid();
+var topic = args._[0] || 'depijp';
+var room = args._[1] || 'wss://de.meething.space:443/'+topic;
+var mesh = websocket(room, {perMessageDeflate: false })
+
+// Create Pub UUID
+var pub = utils.uuid();
+var pubSocket = websocket(room+pub, {perMessageDeflate: false, binary: true})
+var hello = { uuid: uuid, room: room, pub: pub, type: 0 };
+mesh.write(Buffer.from(JSON.stringify(hello)));
+
+// Create Sub UUIDs
+var pipe = false;
+mesh.on('data', function(data){
+  // STDIN input to PubSocket
+  if (!pipe) { process.stdin.pipe(pubSocket); pipe = true; }
+  try {
+	var msg = JSON.parse(data);
+	if (!msg.uuid || msg.uuid == uuid) return;
+	if (swarm.has(msg.uuid)){
+		if (msg.pub == false) swarm.delete(msg.uuid);
+	} else {
+		var subSocket = websocket(room+msg.pub);
+		subSocket.pipe(process.stdout);
+		swarm.set(msg.uuid, subSocket);
+		mesh.write(Buffer.from(JSON.stringify(hello)));
+	}
+  } catch(e) { console.log(e);}
+
+})
+
+mesh.on('end', () => {
+  // reconnect on end
+  mesh = websocket(room, {perMessageDeflate: false })
+});
+
+process.on('SIGINT', function() {
+    hello.pub = false;
+    var exit = mesh.write(Buffer.from(JSON.stringify(hello)));
+    process.exit();
+});
